a04a13a1194f95af9fe118f41a5e55aa0195bb92
Juliop1980 <14-10820@usb.ve>
2022-06-29 15:14:18 +0200

    results of neural network with count vectorization

--- feed_forward_neural_network.py
+++ feed_forward_neural_network.py
@@ -5,6 +5,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from keras import layers
+from keras.models import Sequential
+from keras.backend import clear_session
+from keras.layers import Dense, Softmax, Dropout
+import matplotlib.pyplot as plt
+
+
+plt.style.use('ggplot')
 #from sklearn.preprocessing import MinMaxScaler
 #import matplotlib.pyplot as plt
 #from tensorflow import keras
@@ -17,6 +24,25 @@ from keras import layers
 
 #For jupyter notebook uncomment next line
 #%matplotlib inline
+
+def plot_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
 
 # read csv input data
 df = pd.read_csv('preprocessed_data.csv')
@@ -71,6 +97,22 @@ input_dim = X_train.shape[1]  # Number of features
 model = Sequential()
 model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
+#model.add(Dropout(0.2))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
+#print(model.summary())
+
+history = model.fit(X_train, y_train, epochs=80,verbose=False,validation_data=(X_test, y_test),batch_size=100)
+
+clear_session()
+
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+
+print("Training Accuracy: {:.4f}".format(accuracy))
+
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+plot_history(history)
+plt.savefig('Neural_Network_results/NNmodel_count_vectorizerresults_.png')
