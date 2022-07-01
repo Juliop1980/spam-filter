3fd204cd7f9dfaebc06a3358ef95dc8a773fa3de
Tettoni, Anne Laure Olga <tettoni@campus.tu-berlin.de>
2022-07-01 13:01:47 +0200

    bayes

--- bayes.py
+++ bayes.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -16,50 +15,58 @@ dataTypeDict = dict(df.dtypes)
 
 X = df.iloc[:, 0].values
 Y = df.iloc[:, 1].values
-# print(X)
-# print(Y)
 df["spam"] = df["spam"].astype('category').cat.codes
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-"""print(X_train)
-print(Y_train)
-print(X_test)
-print(Y_test)
-train, test = train_test_split(df, test_size=0.2)
-X_train = train.iloc[:, 0]
-
-Y_train = train.iloc[:, 1]
-# print(X)
-# print(Y)
-X_test = test.iloc[:, 0]
-Y_test = test.iloc[:, 1]
-"""
 
 # scaling input data
-sc_X = StandardScaler()
-# X_train = sc_X.fit_transform(X_train)
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(Y_train)
 # X_test = sc_X.fit_transform(X_test)
 
 training_set = pd.DataFrame({'X': X_train, 'Y': Y_train})
 
-vectorizer = CountVectorizer()
-count_vectorizer = vectorizer.fit_transform(training_set['X'].values)
-
-bigram_vectorizer = TfidfVectorizer(ngram_range=(2, 2))
-count_tfidf = bigram_vectorizer.fit_transform(training_set['X'].values)
-
 # init Multinomial naive bayes
 classifier = MultinomialNB()
+# spam_detect_model = classifier.fit(X_train, Y_train)
+
+# Vectorization model : Count vectorizer -
+vectorizer = CountVectorizer()
+count_vectorizer = vectorizer.fit_transform(training_set['X'].values)
 
 # training the model
 classifier.fit(count_vectorizer, Y_train)
 
-test_counts = vectorizer.transform(X_test)
-predictions = classifier.predict(test_counts)
-# print(predictions)
-# testing the model
-
-
-
-accuracy_count_vect = accuracy_score(Y_test, predictions)
+test_count_vectorizer = vectorizer.transform(X_test)
+predictions_1 = classifier.predict(test_count_vectorizer)
+# testing the accuracy of the model
+accuracy_count_vect = accuracy_score(Y_test, predictions_1)
 print(accuracy_count_vect)
+
+
+# Vectorization model : tf-idf
+bigram_vectorizer = TfidfVectorizer()
+count_tfidf = bigram_vectorizer.fit_transform(training_set['X'].values)
+
+classifier_tfidf = MultinomialNB()
+
+# training the model
+classifier_tfidf.fit(count_tfidf, Y_train)
+
+test_count_tfidf = bigram_vectorizer.transform(X_test)
+predictions_tf = classifier_tfidf.predict(test_count_tfidf)
+
+accuracy_count_tfidf = accuracy_score(Y_test, predictions_tf)
+print(accuracy_count_tfidf)
+
+# vectorization model : countvectorizer - bag of 2-gram
+
+bigram_vectorizer_2 = CountVectorizer(ngram_range=(2, 2))
+X = bigram_vectorizer_2.fit_transform(training_set['X'])
+classifier_count_2 = MultinomialNB()
+
+classifier_count_2.fit(X, Y_train)
+test_count_2 = bigram_vectorizer_2.transform(X_test)
+predictions_2 = classifier_count_2.predict(test_count_2)
+accuracy_count_2 = accuracy_score(Y_test, predictions_2)
+print(accuracy_count_2)
